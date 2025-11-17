b3b98f027961bdad0513d065134cd2ee4881eee3
BarudinXploit <holilhsyhd@gmail.com>
2025-11-18 02:54:08 +0700

    Delete api/create.js

--- api/create.js
+++
@@ -1,8 +0,0 @@
-// api/create.js
-// Hanya info, supaya tidak bingung endpoint mana yang dipakai
-export default function handler(req, res) {
-  return res.status(200).json({
-    message:
-      "Gunakan /api/create-panel untuk panel + server, atau /api/create-admin untuk admin panel.",
-  });
-}
